9e67862bdde861f7b67f858cff8367d24122460d
zeynep <zeynep@DESKTOP-6OT9AEL.mshome.net>
2024-05-01 08:58:45 +0300

    PatikaDev Pratikler

---
+++ src/algorithms/Main.java
@@ -0,0 +1,17 @@
+package algorithms;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Merhaba Dünya");
+		byte b = 0;
+		int a  = b++;
+		int c=-12,d=-5;
+		int e = c%d;
+		System.out.print(e);
+		
+
+	}
+
+}
