8dc864348dab2f07ee3c554ac8d0af5c1edb2253
zeynep <zeynep@DESKTOP-6OT9AEL>
2024-05-06 09:22:32 +0300

    Mükemmel Sayı Bulan Program

---
+++ src/patikadev/ch05/OMukemmelSayi.java
@@ -0,0 +1,29 @@
+package patikadev.ch05;
+
+import java.util.Scanner;
+
+public class OMukemmelSayi {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num,total = 0;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Bir sayı giriniz: ");
+		num = input.nextInt();
+		
+		for(int i=1;i<num;i++) {
+			if(num%i==0) {
+				total += i;
+			}
+		}
+		//System.out.print(total);
+		if(num == total) {
+			System.out.print(num + " Mükemmel Sayıdır");
+		}else {
+			System.out.print(num + " Mükemmel Sayı Değildir");
+		}
+	}
+
+}
