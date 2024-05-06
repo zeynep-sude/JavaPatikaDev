5ef44a6d5a59e1cf5050b21a860814372d344903
zeynep <zeynep@DESKTOP-6OT9AEL>
2024-05-06 09:33:24 +0300

    Ters Üçgen Yapımı

---
+++ src/patikadev/ch05/OTersUcgenYapma.java
@@ -0,0 +1,25 @@
+package patikadev.ch05;
+
+import java.util.Scanner;
+
+public class OTersUcgenYapma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int num;
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Basamak SAYISINI giriniz: ");
+		num = input.nextInt();
+		
+		for(int i=num;i>=1;i--) {
+			for(int j=0;j<2*i-1;j++) {
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
+
+	}
+
+}
