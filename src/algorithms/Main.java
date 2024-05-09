eb2cedc167f65dbb05ce78e8bb2fb8f4517ac92c
zeynep <zeynep@DESKTOP-6OT9AEL>
2024-05-09 10:05:44 +0300

    Ders Tekrarı

--- src/algorithms/Main.java
+++ src/algorithms/Main.java
@@ -12,7 +12,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		recursiveMethod(4);
+		//recursiveMethod(4);
+		int[] scores = new int[10];
+        scores = new int[3];
+        scores = new int[]{215, 234, 218, 189, 221, 290};
+
+        for (int score : scores) {
+            System.out.print(score + "  ");
+        }
 		
 
 
