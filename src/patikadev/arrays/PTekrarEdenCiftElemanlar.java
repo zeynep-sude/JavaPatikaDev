88d02de2c74303c62d9529cb38b2e1dced1e2d69
zeynep <zeynep@DESKTOP-6OT9AEL>
2024-05-10 08:25:11 +0300

    Bir sayı dizisindeki tekrar eden çift sayıları belirten bir program

---
+++ src/patikadev/arrays/PTekrarEdenCiftElemanlar.java
@@ -0,0 +1,43 @@
+package patikadev.arrays;
+
+import java.util.Arrays;
+
+public class PTekrarEdenCiftElemanlar {
+	static boolean isFind(int[] arr,int value) {
+		for(int i:arr) {
+			if(i==value) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr = {1,1,5,1,1,9,8,7,5,3,4,4,9,7,8,6,8};
+		int[] duplicate = new int[arr.length];
+		int starIndex = 0;
+		
+		for(int i=0;i<arr.length;i++) {
+			for(int j=0;j<arr.length;j++) {
+				if((i!=j)&& (arr[i]==arr[j])&&(arr[i]%2==0)) {
+					
+					if(!isFind(duplicate,arr[i])) {
+						duplicate[starIndex++] = arr[i];
+						
+					}
+					break;
+					
+				}
+			}
+		}
+		for(int i:duplicate) {
+			if(i!=0) {
+				System.out.print(i+" ");
+			}
+		}
+		
+
+	}
+
+}
